18213e636110e687ec67952fbdf2616bf2a583e4
doud94220 <edouard.anthony@gmail.com>
2017-06-26 15:57:07 +0200

    ok la page index.php avec les 2 formulaires (independants)

--- index.php
+++ index.php
@@ -3,13 +3,20 @@ require_once('inc/init.inc.php');
 require_once('inc/haut.inc.php');
 
 ?>
-
+	
+				<!-- 	
+					PAS DE COMBINAISON DES 2 FORMULAIRES : C'est l'un ou l'autre (pour l'instant)
+					Pour l'indstant je limite toutes les requetes issues des formulaires à 3 lignes (cf. LIMIT 3)
+				-->
+	
 
 	<div class="conteneurFlex">
 
 		<div id="formGauchePageAccueil">
 			<!-- FORMULAIRE A GAUCHE PAGE D'ACCUEIL -->
 			<form method="post" action="index.php">
+
+				<input type="hidden" name="formNumero1" value="vrai"><br><br> <!--  Pour identifier que c'est le premier form -->
 
 				<label for="categorie">Catégorie : </label><br>
 				<select name="categorie">
@@ -55,7 +62,9 @@ require_once('inc/haut.inc.php');
 			<!-- FORMULAIRE TRIE PRIX PAGE D'ACCUEIL -->
 			<form method="post" action="index.php">
 
-				<select name="trieProduits">
+				<input type="hidden" name="formNumero2" value="vrai"><br><br> <!--  Pour identifier que c'est le deuxieme form -->
+
+				<select name="trieAnnonces">
 					<option value="trieParPrixMoinsCherAuPlusCher">Trier par prix (du moins cher au plus cher)</option>
 					<option value="trieParPrixPlusCherAuMoinsCher">Trier par prix (du plus cher au moins cher)</option>
 					<option value="trieParDatePlusAnciennePlusRecente">Trier par date (de la plus ancienne à la plus récente)</option>
@@ -68,7 +77,7 @@ require_once('inc/haut.inc.php');
 			</form><br><br>
 
 			<?php
-				if($_POST) //Validation des data du premier form
+				if(!empty($_POST['formNumero1'])) //Validation des data du form tout à gauche
 				{
 					////////////// CONTROLES DATA DU POST //////////////
 					// A faire plus tard
@@ -126,13 +135,103 @@ require_once('inc/haut.inc.php');
 							</div>
 							";
 					}
-				} //Fin du traitement des date du POST
+				} //Fin du traitement des données du form 1
+
+				elseif(!empty($_POST['formNumero2'])) //Validation des data du form du milieu avec les 4 filtrages au choix
+				{
+					$trieAnnonces = $_POST['trieAnnonces'];
+					
+					//Requete en BDD
+					$requeteFiltrante = "";
+					switch ($trieAnnonces)
+					{
+						case "trieParPrixMoinsCherAuPlusCher" : 
+							$requeteFiltrante = "SELECT * FROM annonce order by prix asc limit 3";
+							break;
+						case "trieParPrixPlusCherAuMoinsCher" :
+							$requeteFiltrante = "SELECT * FROM annonce order by prix desc limit 3";
+							break;
+						case "trieParDatePlusAnciennePlusRecente" :
+							$requeteFiltrante = "SELECT * FROM annonce order by date_enregistrement asc limit 3";
+							break;
+						case "trieParDatePlusRecentePlusAncienne" :
+							$requeteFiltrante = "SELECT * FROM annonce order by date_enregistrement desc limit 3";
+							break;
+						case "trieParMeilleurVendeur" :
+							$requeteFiltrante = "select * from annonce
+												 where membre_id in
+								                    (select membre_id2 as id_membre_note
+								                     from note
+								                     group by membre_id2
+								                     order by avg(note) desc
+													)
+												limit 3
+													";
+							break;
+					}
+
+					$resultatForm2 = $pdo->query($requeteFiltrante);
+					if ($resultatForm2->rowCount() <= 0) //Si pas de ligne retournée par requête ci-dessus
+					{
+						echo "Pas de résultats pour vos critères de filtrage.";
+					}
+					else //Afficher les annonces dans un tableau dans la partie de droite de la page
+					{
+						echo"<div id='produitsPageAccueil'>
+								<table>
+							";
+
+						while ($annoncesFiltrees = $resultatForm2->fetch(PDO::FETCH_ASSOC))
+						{
+							$urlPhoto = $annoncesFiltrees['photo'];
+							$titre = $annoncesFiltrees['titre'];
+							$idAnnonce =  $annoncesFiltrees['id_annonce'];
+							$descriptionCourte = $annoncesFiltrees['description_courte'];
+							$prix = $annoncesFiltrees['prix'];
+							$idMembre = $annoncesFiltrees['membre_id'];
+
+							//Recup du pseudo du vendeur
+							$resultatPseudoVendeur = $pdo->query("SELECT pseudo from membre where id_membre = '$idMembre'");
+							$arrayPseudoVendeur = $resultatPseudoVendeur->fetch(PDO::FETCH_ASSOC);
+							$pseudoVendeur = $arrayPseudoVendeur['pseudo'];
+
+							//Recup de la note moyenne du vendeur de l'annonce
+							$resultatNoteMoyenneVendeur = $pdo->query("SELECT round(AVG(note),1) AS note_moyenne from note where membre_id2 = '$idMembre'");
+							$arrayNoteMoyenneVendeur = $resultatNoteMoyenneVendeur->fetch(PDO::FETCH_ASSOC);
+							$noteMoyenneVendeur = $arrayNoteMoyenneVendeur['note_moyenne'];
+
+							echo"
+									<tr>
+										<td><img src='" . $urlPhoto . "' width='100px' height='80px'></td>
+										<td>
+											<p><a href='ficheAnnonce.php?id_annonce=" . $idAnnonce . "'>" . $titre . "</a></p>
+											<p>" . $descriptionCourte . "</p>
+											<p>" . $pseudoVendeur . " : " . $noteMoyenneVendeur . "</p>
+										</td>
+										<td>
+											<p>" . $prix . "€ </p>
+										</td>
+									</tr>
+							 	";
+						}
+						echo"	</table>
+							</div>
+							";
+					}
+
+				} //Fin du traitement des données du form 2
+
+				else //Si aucun formulaire ,'a été validé précédemment, alors afficher "Bienvenue"
+				{
+					echo "Bienvenue sur notre site !";
+				}
 			?>
 		
 
 		</div> <!-- Fin partiePrincipalePageAccueil -->
 
 	</div> <!-- Fin conteneurFlex" -->
+
 
 
 <?php
